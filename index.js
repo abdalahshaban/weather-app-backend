35ccd942d7dfb92ff1420e7ec71dc0227ff50666
unknown <abdalahshaban@yahoo.com>
2019-10-16 18:22:22 +0200

    make DB connection

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dbConfig = require('./src/config/DB');
 const app = express();
 
+const PORT = process.env.PORT || 5000
+//Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//connection DB
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.URL, { useUnifiedTopology: true, useNewUrlParser: true }, err => {
+    if (!err) {
+        console.log('connected to DB IS Ok');
+    } else {
+        console.log('Error ', JSON.stringify(err));
+    }
+}
+);
+
+
+app.listen(PORT, () => {
+    console.log(`Server has start on port ${PORT}`);
+})
---
+++ src/config/DB.js
@@ -0,0 +1,3 @@
+module.exports = {
+    URL: 'mongodb+srv://weatherapp:12345@weatherapp-2h8y4.mongodb.net/test?retryWrites=true&w=majority'
+};
