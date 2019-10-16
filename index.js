42eb87b36c102c718c359aa07f9a04f2c7c07f39
unknown <abdalahshaban@yahoo.com>
2019-10-16 18:04:44 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
