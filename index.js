7b7a39f1f0fc91be19e3d4e466da9953094f0bea
unknown <abdalahshaban@yahoo.com>
2019-10-17 03:41:52 +0200

    deploy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dbConfig = require('./src/config/DB');
 const fetch = require('./src/helpers/fetchData')
+const path = require('path')
 const app = express();
 
 const PORT = process.env.PORT || 5000
@@ -11,6 +12,12 @@ const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'src', 'public')))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'public/index.html'))
+});
 
 //connection DB
 
